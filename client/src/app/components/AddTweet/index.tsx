86a82bd81ac7b7f3d0bfa38a52f5354ee6f97467
1234bhaskar <bhaskar2021chand@gmail.com>
2024-07-12 21:05:42 +0530

    Mobile Responsive

--- client/src/app/components/AddTweet/index.tsx
+++ client/src/app/components/AddTweet/index.tsx
@@ -23,15 +23,15 @@ export const AddTweet = ({user}:any):React.ReactNode => {
   },[content,mutate]);
   return (
     
-            <div className='grid grid-cols-12 gap-3 p-5'>
-            <div className='col-span-1 '>
+            <div className='grid grid-cols-12 gap-3 p-2 py-5  sm:p-5'>
+            <div className='col-span-2 sm:col-span-1 '>
               {
                 user?.profileImageURL &&(
                  user && user.profileImageURL && (<Image className='rounded-full' src={user?.profileImageURL} alt="user-image"  height={50} width={50} />)
                 )
               }
             </div>
-            <div className='col-span-11'>
+            <div className='col-span-10 sm:col-span-11'>
               <textarea value={content} onChange={(e)=>setContent(e.target.value)} placeholder={"What's Happening?"} rows={3} className=' px-1 bg-transparent border-b border-slate-700 w-full text-xl '>
               </textarea>
             <div className='flex justify-between px-4 my-2 items-center'>
--- client/src/app/components/FeedCard/index.tsx
+++ client/src/app/components/FeedCard/index.tsx
@@ -14,13 +14,13 @@ interface FeedCardProps{
 export const FeedCard:FC<FeedCardProps> = (props) => {
   const {data}= props
   return (
-    <div className='border-t  border-gray-600 p-5  transition-all cursor-pointer'>
+    <div className='border-t  border-gray-600  py-5 p-2 sm:p-5  transition-all cursor-pointer'>
       <div className='grid grid-cols-12  gap-3'>
-      <div className='col-span-1'>
+      <div className='col-span-2 sm:col-span-1'>
         {
           data.author?.profileImageURL && <Image className='rounded-full' src={data.author.profileImageURL} alt='avavtar' height={60} width={150} />}
       </div>
-      <div className='col-span-11'>
+      <div className='col-span-10 sm:col-span-11'>
         <h5>{data.author?.firstName+" "+data.author?.lastName}</h5>
         <p>{data.content}</p>
         <div className='flex  justify-between p-2 w-[90%] text-gray-500'>
