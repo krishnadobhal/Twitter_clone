617ea3044c4b343391b460a4b486e9287d78a0ca
1234bhaskar <bhaskar2021chand@gmail.com>
2024-07-12 10:10:02 +0530

    Create Tweet functionality on FrontEnd

--- client/src/app/components/AddTweet/index.tsx
+++ client/src/app/components/AddTweet/index.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 import { CiImageOn } from "react-icons/ci";
+import { useCreateTweet } from '../../../../hooks/tweet';
 
 
 export const AddTweet = ({user}:any):React.ReactNode => {
-
+ 
+    const [content,setContent]=useState('');
   const handleSelectImage=useCallback(()=>{
     const input=document.createElement('input');
     input.setAttribute('type',"file");
@@ -12,22 +14,29 @@ export const AddTweet = ({user}:any):React.ReactNode => {
     input.click();
   },[])
 
+  const {mutate} =useCreateTweet();
+
+  const handleCreateTweet= useCallback(()=>{
+   mutate({
+    content,
+   });
+  },[content,mutate]);
   return (
     
-            <div className='grid grid-flow-col grid-col-12 gap-3 pt-4'>
-            <div className='col-span-1 h-fit w-fit rounded-3xl overflow-hidden ml-3'>
+            <div className='grid grid-cols-12 gap-3 p-5'>
+            <div className='col-span-1 '>
               {
                 user?.profileImageURL &&(
-                 user && user.profileImageURL && (<Image src={user?.profileImageURL} alt="user-image"  height={50} width={50} />)
+                 user && user.profileImageURL && (<Image className='rounded-full' src={user?.profileImageURL} alt="user-image"  height={50} width={50} />)
                 )
               }
             </div>
-            <div className='col-span-11 pr-5'>
-              <textarea placeholder={"What's Happening?"} rows={3} className=' px-1 bg-transparent border-b border-slate-700 w-full text-xl '>
+            <div className='col-span-11'>
+              <textarea value={content} onChange={(e)=>setContent(e.target.value)} placeholder={"What's Happening?"} rows={3} className=' px-1 bg-transparent border-b border-slate-700 w-full text-xl '>
               </textarea>
             <div className='flex justify-between px-4 my-2 items-center'>
               <CiImageOn onClick={handleSelectImage} className='text-2xl '/>
-              <button className='bg-blue-500 py-2 rounded-3xl px-6 font-bold'>Post</button>
+              <button onClick={handleCreateTweet} className='bg-blue-500 py-2 rounded-3xl px-6 font-bold'>Post</button>
             </div>
             </div>
             </div>     
