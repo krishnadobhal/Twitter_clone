34bf8c657c357276533058a68be7ebc400889aae
1234bhaskar <bhaskar2021chand@gmail.com>
2024-07-12 10:06:45 +0530

    Created hooks for Creating Tweet(useCreateTweet) and Fetching all tweets(useGetAllTweets)

---
+++ client/hooks/tweet.ts
@@ -0,0 +1,29 @@
+import toast from "react-hot-toast";
+import { graphqlClient } from "../clientgrahql/api";
+import { CreateTweetData } from "../gql/graphql";
+import { createTweetMutation } from "../graphql/mutation/tweet";
+import { getAllTweetsQuery } from "../graphql/query/tweet";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+export const useCreateTweet =()=>{
+    const queryClient=useQueryClient();
+
+    const mutation=useMutation({
+        mutationFn:(payload:CreateTweetData)=>
+            graphqlClient.request(createTweetMutation,{payload}),
+        onMutate:(payload)=>toast.loading("Creating Tweet",{id:'1'}),
+        onSuccess:async()=>{
+            await queryClient.invalidateQueries(["all-tweets"]);
+            toast.success('Created Success',{id:'1'})
+        },
+    })
+    return mutation;
+}
+
+export const useGetAllTweets = ()=>{
+    const query=useQuery({
+        queryKey: ["all-tweets"],
+        queryFn: () => graphqlClient.request(getAllTweetsQuery)
+    });
+    return {...query,tweets:query.data?.getAllTweets};
+};
