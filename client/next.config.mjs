85a4451b6e47ceb5c5a9082d629cde623954fb76
1234bhaskar <bhaskar2021chand@gmail.com>
2024-07-24 18:54:46 +0530

    Loading Spinner added while Loading

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['avatars.githubusercontent.com','lh3.googleusercontent.com','krishba-twitter-dev.s3.ap-south-1.amazonaws.com'],
-    },
+  images: {
+    domains: [
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
+      "krishna-twitter-dev.s3.ap-south-1.amazonaws.com",
+    ],
+  },
 };
 
 export default nextConfig;
